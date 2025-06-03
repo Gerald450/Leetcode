e3c49752a6483163711202233144c80ac2b15b77
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-06-03 11:32:23 -0400

    Time: 3 ms (3.93%) | Memory: 18 MB (11.09%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,12 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-
-
-      
-
-        while val in set(nums):
-            nums.remove(val)
-        
-        print(nums)
-        return len(nums)
-        
+        k = 0  # Index to place the next non-val element
+        for i in range(len(nums)):
+            print(i, k, nums)
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
