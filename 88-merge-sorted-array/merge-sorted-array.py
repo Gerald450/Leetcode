50d74a2c3bd1d62b4ffcf2eededcdf41e49aa433
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-06-02 21:41:05 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,23 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # Start from the end
-        p1 = m - 1  # Pointer for nums1
-        p2 = n - 1  # Pointer for nums2
-        p = m + n - 1  # Pointer for placement in nums1
-
-        # Merge from the back
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
-
-        # Only need to check if nums2 has remaining elements
-        while p2 >= 0:
-            nums1[p] = nums2[p2]
-            p2 -= 1
-            p -= 1
+        r = 0
+        for i in range(m, len(nums1)):
+            nums1[i]=nums2[r]
+            r += 1
+        nums1.sort()
