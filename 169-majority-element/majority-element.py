48056ae2efa7cbe4b5c5e3f311e29713ab6391fc
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-06-05 18:02:40 -0400

    Time: 3 ms (83.29%) | Memory: 19.2 MB (87.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
+        count, res = 0, 0
 
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += 1 if num == candidate else -1
+        for n in nums:
+            res = n if count == 0 else res
+            count += (1 if res == n else -1)
 
-        return candidate
+        return res
