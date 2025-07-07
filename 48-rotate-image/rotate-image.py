6d0ce983e5f551f16852f8bfe9daeec2e1e610c5
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-07-07 15:56:46 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (41.59%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,27 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        l,r = 0, len(matrix) - 1
+        matrix.reverse()
 
-        while l < r:
-            for i in range(r - l):
-                top, bottom = l, r
-
-                #topleft
-                topleft = matrix[top][l + i]
-
-                #topleft = bottomleft
-                matrix[top][l + i] = matrix[bottom - i][l]
-
-                #bottomleft = bottomright
-                matrix[bottom - i][l] = matrix[bottom][r - i]
-
-                #bottomright = topright
-                matrix[bottom][r - i] = matrix[top + i][r]
-
-                #topright = topleft
-                matrix[top + i][r] = topleft
-            
-            l += 1
-            r -= 1
+        for row in range(len(matrix)):
+            for col in range(row+1, len(matrix)):
+                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
 
