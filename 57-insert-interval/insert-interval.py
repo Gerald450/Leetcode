80a60a9a2698ed3b8da6627fef7fb3a29be91de1
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-07-12 12:38:33 -0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (60.64%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,22 +1,26 @@
+from typing import List
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if len(intervals) == 0:
-            return [newInterval]
+        res = []
+        i = 0
+        n = len(intervals)
 
-        intervals.append(newInterval)
+        # 1. Add intervals before newInterval
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
 
-        intervals = sorted(intervals, key=lambda x:x[0])
+        # 2. Merge overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
 
-        stack = [intervals[0]]
+        # 3. Add intervals after newInterval
+        while i < n:
+            res.append(intervals[i])
+            i += 1
 
-        for x, y in intervals[1:]:
-            start, end = stack[-1]
-
-            if end >= x:
-                stack.pop()
-                stack.append([start, max(end, y)])
-            else:
-                stack.append([x, y])
-
-        return stack
-        
+        return res
