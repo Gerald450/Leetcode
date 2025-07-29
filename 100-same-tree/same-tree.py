3f8530cb93ce2b7dec417e96c0d33fc4bbc006c9
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-07-29 15:47:42 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (20.91%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,38 +6,15 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
         if not q and not p:
             return True
+        if not q or not p or p.val != q.val:
+            return False
 
-        arrp = []
-        arrq = []
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
 
-        qq = collections.deque([q])
-        qp = collections.deque([p])
 
-        while qq:
-
-            for _ in range(len(qq)):
-                node = qq.popleft()
-                val = node.val if node else None
-                arrq.append(val)
-                if node:
-                    qq.append(node.left)
-                    qq.append(node.right)
-        
-        while qp:
-
-            for _ in range(len(qp)):
-                node = qp.popleft()
-                val = node.val if node else None
-                arrp.append(val)
-                if node:
-                    qp.append(node.left)
-                    qp.append(node.right)
-
-        print(arrq, arrp)
-
-        return arrp == arrq
             
 
                 
