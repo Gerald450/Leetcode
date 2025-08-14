cc614a8d3f72fcda4607910fb28e6f7f63711a6f
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-08-14 18:24:24 -0500

    Time: 7 ms (54.89%) | Memory: 21.6 MB (88.34%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row, col = len(board), len(board[0])
+
+        def capture(r, c):
+            if (r < 0 or c < 0 or r >= row or c >= col or board[r][c] != 'O'):
+                return
+            
+            board[r][c] = 'T'
+
+            capture(r + 1,c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+
+        #capture unsorrounded regions (o -> t)
+        for r in range(row):
+            for c in range(col):
+                if (board[r][c] =='O' and r in [0, row - 1] or c in [0, col - 1]):
+                    capture(r, c)
+
+        #capture sourrounded regions (o -> x)
+        for r in range(row):
+            for c in range(col):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+
+
+        #uncapture sorrounded regions (t -> o)
+        for r in range(row):
+            for c in range(col):
+                if board[r][c] == 'T':
+                    board[r][c] = 'O' 
+
+        
+
+        
+        
