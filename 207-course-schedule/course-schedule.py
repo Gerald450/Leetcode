9d6371f6126bd904f1b583e708fc59ca17ebf0f2
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-08-28 23:29:36 -0500

    Time: 7 ms (46.07%) | Memory: 19.3 MB (40.78%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = collections.defaultdict(list)
+
+        for p in prerequisites:
+            a, b = p
+            adj[a].append(b)
+
+        visitCrs = set()
+
+        def dfs(crs):
+            if adj[crs] == []:
+                return True
+            if crs in visitCrs:
+                return False
+
+            visitCrs.add(crs)
+            for pre in adj[crs]:
+                if not dfs(pre):
+                    return False
+            visitCrs.remove(crs)
+            adj[crs] = []
+            return True
+
+
+        for p in prerequisites:
+            if not dfs(p[0]): return False
+
+        return True
+            
+
+        
+
+        
+
+
+        
