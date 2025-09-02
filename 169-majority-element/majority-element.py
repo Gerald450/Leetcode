cc1552624a555132c54823441ff80e44702671bf
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-09-02 00:54:13 -0500

    Time: 8 ms (37.19%) | Memory: 19.6 MB (18.84%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count, res = 0, 0
+        hashmap = collections.Counter(nums)
 
-        for n in nums:
-            res = n if count == 0 else res
-            count += (1 if res == n else -1)
-
-        return res
+        for key, value in hashmap.items():
+            if value > len(nums) / 2:
+                return key
