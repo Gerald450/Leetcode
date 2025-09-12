21d0200997c4f55db1db5bca30e126cbcfc49038
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-09-11 23:02:25 -0500

    Time: 0 ms (100%), Space: 18.3 MB (22.72%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -10,17 +10,17 @@ class Solution:
             
             #left sorted portion
             if nums[l] <= nums[m]:
-                if target > nums[m] or target < nums[l]:
-                    l = m + 1
-                else:
+                if nums[l] <= target < nums[m]:
                     r = m - 1
+                else:
+                    l = m + 1
 
             #right sorted portion
             else:
-                if  target < nums[m] or target > nums[r]:
-                    r = m - 1
-                else:
+                if nums[m] < target <= nums[r]:
                     l = m + 1
+                else:
+                    r = m - 1
 
         return -1
 
