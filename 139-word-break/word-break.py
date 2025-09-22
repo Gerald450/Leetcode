5c1e1a7a26de09cd7243d68fa1572fe1b37afbeb
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-09-22 00:39:16 -0500

    Time: 1 ms (77.99%) | Memory: 17.8 MB (64.58%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -3,19 +3,16 @@ class Solution:
         dp = [False] * (len(s) + 1)
         dp[len(s)] = True
 
+
         for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
-                n = len(w)
-                if i + n <= len(s) and s[i:i+n] == w:
-                    dp[i] = dp[i + n]
+                if i + len(w) <= len(s) and s[i: i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
                 if dp[i]:
                     break
+        return dp[0] 
 
-
-                #commit2
-                #commit23
-
-        return dp[0]
         
-   
+
+
         
