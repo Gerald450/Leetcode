d3d66a3593756499e3627627fff0b710b63e6639
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-09-25 00:22:50 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (46.90%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -2,35 +2,43 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
 
         adj = collections.defaultdict(list)
+        otp = []
 
-        for i, val in enumerate(equations):
-            a, b = val
-            adj[a].append([b, values[i]])
-            adj[b].append([a, 1/values[i]])
+        for idx, eq in enumerate(equations):
+            a, b = eq
+            adj[a].append([b, values[idx]])
+            adj[b].append([a, 1/values[idx]])
+        
+        
 
-        def bfs(numero, target):
-            if numero not in adj or target not in adj:
+        def bfs(curr, target):
+            if target not in adj or curr not in adj:
                 return -1
 
-            q, visit = deque(), set()
-            q.append([numero, 1])
+            q = collections.deque()
+            visited = set()
+            q.append([curr, 1])
+            visited.add(curr)
 
             while q:
                 n, w = q.popleft()
+
                 if n == target:
                     return w
                 for nei in adj[n]:
-                    l, weight = nei
-                    if l not in visit:
-                        q.append([l, w * weight])
-                        visit.add(l)
+                    node, weight = nei
+                    
+                    if node not in visited:
+                        q.append([node, w * weight])
+                        visited.add(node)
+
             return -1
 
 
-        return [bfs(q[0], q[1]) for q in queries]
-
-
-            
+        for q in queries:
+            otp.append(bfs(q[0], q[1]))
+        
+        return otp
 
 
 
