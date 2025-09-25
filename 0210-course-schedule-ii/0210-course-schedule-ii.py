eed74ad35969495bb3049554dd5b1d44e13b1aaf
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-09-24 22:02:02 -0500

    Time: 3 ms (75.97%), Space: 20 MB (6.04%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,36 @@
+import collections
+from typing import List
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = collections.defaultdict(list)
+
+        for pre in prerequisites:
+            a, b = pre
+            adj[b].append(a)
+
+        otp = []
+        visiting, visited = set(), set()
+        def dfs(course):
+            if course in visiting:
+                return False
+            if course in visited:
+                return True
+
+            visited.add(course)
+            visiting.add(course)
+
+            for nei in adj[course]:
+                if not dfs(nei):
+                    return False
+            visiting.remove(course)
+            otp.append(course)
+            return True
+
+
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return otp[::-1]
