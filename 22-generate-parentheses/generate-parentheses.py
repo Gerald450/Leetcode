923030db8f6338fd55db6a1adf0eb09379f93463
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-09-30 17:30:09 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (25.47%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        #add open brackets only if open < n
+        #add closed brackets only if closed < open
+        #valid if open == closed == n
+        stack = []
+        otp = []
+       
+        def backtrack(openN, closedN):
+            
+            if openN == closedN == n:
+                otp.append("".join(stack))
+                return otp
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1,closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN,closedN+1)
+                stack.pop()
+        backtrack(0,0)
+        return otp
