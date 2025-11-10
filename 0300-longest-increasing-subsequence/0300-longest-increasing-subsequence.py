67f3a45ec017e98793f8f2508b3d234f6421f068
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-11-09 21:47:16 -0500

    Time: 1781 ms (35.59%), Space: 18.2 MB (37.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,14 +1,44 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        '''
+        output: LIS -> int
+
+        edge: negative nums, repeated nums
+
+        initialise dp -> [1] at each point
+        use dp arr to store lis at each num in nums, start at the end
+        run a loop at each point comparing nums
+            if num is less than curr num, then get max(dp[curr], 1 + dp[i])
+        return max(dp)
+
+        '''
         dp = [1] * len(nums)
 
-        for i in range(len(nums) - 1, -1, -1):
-            for r in range(i + 1, len(nums)):
-                if nums[i] < nums[r]:
-                    dp[i] = max(dp[i], 1 + dp[r])
+        for c in range(len(nums) - 1, -1, -1):
 
+            for i in range(c + 1, len(nums)):
+                if nums[i] > nums[c]:
+                    dp[c] = max(dp[c], 1 + dp[i])
+            
         return max(dp)
         
+        '''
+        [10,9,2,5,3,7,101,18]
+                  ^
+        
+        dp = [2, 2, 4, 3, 3, 2, 1, 1]
+        ix = [0, 1, 2, 3, 4, 5, 6, 7]
+
+        c = 4 -> 3
+            i = 6 -> 101
+            101 > 3
+            
+
+
+
+
+        '''
+
         
 
                 
