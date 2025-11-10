7cf7216646bda93460a98cd296400ac3a45d6c1f
Gerald450 <157247952+Gerald450@users.noreply.github.com>
2025-11-09 20:51:17 -0500

    Time: 0 ms (100%), Space: 17.7 MB (54.12%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,20 +1,50 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        '''
+        input: string
+        output: decoded string
+
+        use stack
+        Fo r
+        put in stack until ]
+        start poping until [, save that in substr
+        pop [
+        pop while isDigit and inbound
+        save that in num
+        add num * substr to stack
+
+        return stack
+
+        '''
+
         stack = []
-        for i in range(len(s)):
-            if s[i] != ']':
-                stack.append(s[i])
-            else:
-                substr = ""
+
+        for char in s:
+            if char == ']':
+                substr = ''
                 while stack[-1] != '[':
                     substr = stack.pop() + substr
                 stack.pop()
-                num = ""
-                
+                num = ''
                 while stack and stack[-1].isdigit():
                     num = stack.pop() + num
                 
                 stack.append(int(num) * substr)
+            else:
+                stack.append(char)
+        
+        return ''.join(stack)
 
 
-        return "".join(stack)
+
+'''
+3[a2[c]]
+       ^
+stack = [accaccacc]
+substr = 'acc'
+num = '3'
+
+
+
+
+'''
