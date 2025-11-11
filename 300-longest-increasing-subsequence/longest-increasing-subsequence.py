97fee7a0290cf9e9440d8490a17905d6a66910ea
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-10 20:10:53 -0500

    Time: 11 ms (78.91%) | Memory: 18.2 MB (37.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,14 +1,59 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
+        '''
+        output: LIS -> int
 
-        for i in range(len(nums) - 1, -1, -1):
-            for r in range(i + 1, len(nums)):
-                if nums[i] < nums[r]:
-                    dp[i] = max(dp[i], 1 + dp[r])
+        edge: negative nums, repeated nums
 
+        initialise dp -> [1] at each point
+        use dp arr to store lis at each num in nums, start at the end
+        run a loop at each point comparing nums
+            if num is less than curr num, then get max(dp[curr], 1 + dp[i])
         return max(dp)
+
+        '''
+        dp = []
+
+        def binary_search(num, arr):
+            l, r = 0, len(arr) - 1
+
+            while l <= r:
+                m = (l + r) // 2
+                if arr[m] < num:
+                    l = m + 1
+                elif arr[m] > num:
+                    r = m - 1
+                else:
+                    return m
+
+            return l
+
+        for num in nums:
+            length = len(dp)
+
+            pos = binary_search(num, dp)
+
+            if pos == length:
+                dp.append(num)
+            else:
+                dp[pos] = num
+
+        return len(dp)
         
+        '''
+        [10,9,2,5,3,7,101,18]
+                  ^
+        
+        dp = [2, 2, 4, 3, 3, 2, 1, 1]
+        ix = [0, 1, 2, 3, 4, 5, 6, 7]
+
+        c = 4 -> 3
+            i = 6 -> 101
+            101 > 3
+            
+
+        '''
+
         
 
                 
