0d224e3d7d46f180f693b9f1adc0e6fc4478bdaf
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-11 14:33:21 -0600

    Time: 7 ms (50.21%) | Memory: 21.4 MB (28.07%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,53 @@
+from collections import Counter
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        input: nums -> arr, k -> int
+        output: top k most freq
+
+        edge cases: same freq
+
+        plan:
+        make a freq dict
+        make a maxheap -> [(-freq, num)]
+        pop k times, insert to otp
+        return otp
+
+        '''
+
+        freqDict = Counter(nums)
+
+        maxHeap = []
+
+        for key, value in freqDict.items():
+            maxHeap.append((-value, key))
+
+        heapq.heapify(maxHeap)
+        otp = []
+
+        for i in range(k):
+            _, key = heapq.heappop(maxHeap)
+            otp.append(key)
+
+        return otp
+
+        '''
+        freqDict = {
+            1: 4,
+            2: 4,
+            3: 2
+        }
+
+        maxHeap = [(-4, 2), (-2, 3)]
+        heap = (-4, 2)
+
+        i = 2 -> 2
+        otp = [1, 2]
+
+
+
+        '''
+
+
+        
