b6a9bce5ef86bf9db8db547de052c0b55dd473a2
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-10 20:02:12 -0500

    Time: 255 ms (19.11%), Space: 53 MB (34.43%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -1,24 +1,70 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        envelopes.sort(key=lambda x:(x[0], -x[1]))
+        '''
+        output: max num of envelopes that can get into each other including the outside one
 
-        heights = [h for _, h in envelopes]
+        edge cases: empty return 0
+                    all same return 1
 
+
+        sort them by width, if they same, sort by h in reverse [[2,3], [5,4], [6,7], [6,4]]
+        count starts at 1
+        compare w and h if they all less increment count
+        do lis
+
+        '''
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
         dp = []
 
-        for height in heights:
-            idx = bisect_left(dp, height)
+        def binary_search(num, arr):
+            l, r = 0, len(arr) - 1
 
-            if idx == len(dp):
-                dp.append(height)
+            while l <= r:
+                m = (l + r) // 2
+                if arr[m] < num:
+                    l = m + 1
+                elif arr[m] > num:
+                    r = m - 1
+                else:
+                    return m
+            
+            return l
+
+        for w, h in envelopes:
+            length = len(dp)
+
+            # while i < length:
+            #     if h <= dp[i]:
+            #         break
+            #     i += 1
+
+            pos = binary_search(h, dp)
+            
+            if pos == length:
+                dp.append(h)
             else:
-                dp[idx] = height
-
+                dp[pos] = h
+        
         return len(dp)
 
-            
 
-        
+
+        '''
+        [[1,3],[3,5],[6,7],[6,8],[8,4],[9,5]]
+
+        envelopes = [[1,3], [3,5], [6,8], [6,7], [8,4], [9,5]]
+                                                   ^ 
+        prev = [6, 8]
+        count = 3
+        w, h = 6, 7
+        prevW, prevH = 6, 8
+
+
+
+        '''
+
+    
+    
         
         
                 
