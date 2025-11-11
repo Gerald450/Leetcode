0febf25cafa84de1cf01c74f15508c23be9fa095
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-10 23:31:53 -0600

    Time: 253 ms (47.22%), Space: 29 MB (7.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,46 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        '''
+        input: grid matrix
+        output: int num of islands
+
+        edge: 
+
+        plan:
+        traverse all connected 1's, add to island, put in hashset to avoid cylces
+        dfs
+
+
+        '''
+
+        seen = set()
         rows, cols = len(grid), len(grid[0])
         islands = 0
-        seen = set()
 
 
-        def dfs(r,c):
+        def dfs(r, c):
+            if r < 0 or r > rows - 1 or c < 0 or c > cols - 1:
+                return 
             if grid[r][c] == '0' or (r,c) in seen:
                 return
+            
+            seen.add((r, c))
 
-            seen.add((r,c))
+            dfs(r, c + 1)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r - 1, c)
 
-            directions = [[1,0], [0,1], [-1,0], [0,-1]]
 
-            for direction in directions:
-                dr, dc = direction
-                row, col = r + dr, c + dc
-                
-                if ((row, col) not in seen and
-                    row in range(0, rows) and
-                    col in range(0, cols) and
-                    grid[row][col] == '1'):
-
-                    dfs(row, col)
-                    seen.add((row, col))
 
 
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r,c) not in seen:
-                    islands += 1
                     dfs(r, c)
+                    islands += 1
+                    
 
+        
         return islands
