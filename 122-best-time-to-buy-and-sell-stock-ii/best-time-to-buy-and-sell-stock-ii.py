019769aac908ce7a1accea922fcb467768e42745
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-10 22:50:45 -0600

    Time: 1 ms (49.39%) | Memory: 18.5 MB (98.41%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,9 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        '''
+        input: prices
+        output: max profit
+
+        edge cases: monotonic decrease, negative, one, zero
+
+        plan:
+
+        compare to previous if less add to profit
+        '''
         profit = 0
 
         for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += (prices[i] - prices[i-1])
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
 
         return profit
+
+            
+
