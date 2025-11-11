168d914a805aea9764da0d2e2d5bed296b65b6e1
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-11 15:39:53 -0600

    Time: 3 ms (77.06%), Space: 19.9 MB (12.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,35 +1,126 @@
-import collections
+from collections import defaultdict
 from typing import List
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = collections.defaultdict(list)
+        '''
+        input: numCourses -> int, prerequisites -> arr of arr
+        output: list of courses in order
 
-        for pre in prerequisites:
-            a, b = pre
-            adj[a].append(b)
-            #0:1
+        edge cases: cycle: return empty, 
 
+        plan:
+        make an adj graph -> {0: [1, 4]} e.tc
+        initialise otp
+        need a set for cycles
+        loop through numCourses:
+            call dfs(course): if false return []
+
+        dfs => {
+            base case: if not in adj: return True
+            if in set: return False
+            if not adj[course]: true
+
+            add to set
+
+            loop through neighbours: if dfs(nei) is false return false
+
+            remove from set
+            make adj[course] empty
+            append to otp
+        }
+
+        return otp
+
+        '''
+
+        adj = defaultdict(list)
         otp = []
-        visiting, visited = set(), set()
+
+        for a, b in prerequisites:
+            adj[a].append(b)
+
+        visiting = set()
+        visited = set()
         def dfs(course):
-            if course in visiting:
-                return False
             if course in visited:
                 return True
-
-            visited.add(course)
+            if course in visiting:
+                return False
+            
+            
             visiting.add(course)
+            
 
             for nei in adj[course]:
                 if not dfs(nei):
                     return False
+
             visiting.remove(course)
+            visited.add(course)
             otp.append(course)
             return True
+
+            
 
         for i in range(numCourses):
             if not dfs(i):
                 return []
 
         return otp
+
+
+        '''
+        adj = {
+            0: 1
+        }
+
+        i = 0 -> 2
+        visited = {0}
+        visiting = {0}
+        otp = [1, 0]
+
+        dfs(0) => {
+            nei = 1 -> 1
+            dfs(1) => {
+                
+            }
+        }
+
+
+
+        adj = {
+            1:[],
+            2:[],
+            3:[1, 2]
+        }
+        visited = {0, 1, 2, 3}
+        visiting = {3}
+        otp = [0, 1, 2, 3]
+        
+        i = 3 -> 4
+        dfs(0) => {
+            True
+        }
+        dfs(1) => {
+            nei = 0 -> 0
+            dfs(0) => {
+                True
+            }
+        }
+
+        dfs(2) => {
+            nei = 0 => 0
+            dfs(0) => {
+                True
+            }
+        }
+
+        dfs(3) => {
+            nei = 2 -> 2
+            dfs
+        }
+
+
+
+        '''
