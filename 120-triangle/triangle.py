6c85138000be107099c42818da8b6dc32eeaa54a
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-10 22:03:34 -0600

    Time: 4 ms (49.79%) | Memory: 18.6 MB (92.19%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,16 +1,48 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        triangle.reverse()
+        '''
+        input: that
+        output: int
+
+        edge cases: empty, one
+
+        plan:
+        start from the bottom, getting the minimum at each index
+        start at -2
+        return arr[0]
+
+        '''
+
+        dp = triangle[-1]
+
+        for i in range(len(triangle) - 2, -1, -1):
+
+            for j in range(len(triangle[i])):
+                dp[j] = min(dp[j] + triangle[i][j], dp[j + 1] + triangle[i][j])
+
+        return dp[0]
 
 
-        for i in range(len(triangle) - 1):
-           
-            for j in range(len(triangle[i]) - 1):
-                minVal = min(triangle[i][j], triangle[i][j+1])
-                triangle[i + 1][j] += minVal
+        '''
+        [[2],[3,4],[6,5,7],[4,1,8,3]]
 
+
+        [[-10]]
+        i = 0
+        j = 0
+
+        triangle[0] = [-10]
+
+        dp = [-10]
         
-        
-        return triangle[-1][0]
+
+
+
+
+
+        '''
+
+
+
 
         
