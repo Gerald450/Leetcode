0f0ed7b646158b7e2367a80d2f5c047f6c5823dc
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-11 14:18:08 -0600

    Time: 8 ms (23.34%) | Memory: 19.2 MB (50.55%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,33 +1,68 @@
+from collections import deque, defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = collections.defaultdict(list)
-        seen = set()
+        '''
+        input: numCourses -> int, prerequisites -> arr of arr
+        output: boolean
 
-        for pre in prerequisites:
-            a, b = pre
+        edge cases: cycle [[1,0], [0,1]]
+
+        plan:
+        make an adj graph: adj[course] = [pre1, pre2.....]
+        loop in range numCourses
+        dfs(course) through adj
+        use set to detect cycles 
+        base case: if course in set: return False
+        at the end return True
+
+        '''
+        adj = defaultdict(list)
+
+        for a, b in prerequisites:
             adj[a].append(b)
-        print(adj[0])
 
-        def dfs(num):
-            if num in seen:
+        visiting = set()
+        
+        def dfs(course):
+            if course in visiting:
                 return False
-            if adj[num] == []:
+            if course not in adj:
+                return True
+            if not adj[course]:
                 return True
 
-            seen.add(num)
+            visiting.add(course)
 
-            for pre in adj[num]:
-                if not dfs(pre):
+            for nei in adj[course]:
+                if not dfs(nei):
                     return False
-            seen.remove(num)
-            adj[num] = []
+            
+            visiting.remove(course)
+            adj[course] = []
             return True
 
-        #commit
 
 
         for i in range(numCourses):
             if not dfs(i):
                 return False
+
         return True
-        
+
+        '''
+        numCourses = 2, pre = [[1,0]]
+
+        adj = {
+            1: 0
+        }
+
+        visiting = {1}
+
+        i = 1 -> 2
+        dfs(1)
+        adj[1] = 0
+            dfs(0)
+            
+
+        '''
+
