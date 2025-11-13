f1e6477b0b82a543566c375de6364d9cf11d6de2
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-13 17:49:01 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.57%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,15 +6,28 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
-        if not q and not p:
+        '''
+        input: two roots, p and q
+        output: boolean, if they are the same
+        Match: Dfs
+        edge cases: p is empty, q is not or vice versa
+        if both empty: return true
+
+        base cases:
+        if not p and not q: true
+        if not p or not q: false
+
+        return p.val == q.val and  traverse q.right and p.right and traverse q.left and p.left
+        '''
+
+        if not p and not q: 
             return True
-        if not q or not p or p.val != q.val:
+        
+        if not p or not q:
             return False
 
-        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
 
-
+        return (p.val == q.val and self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left))
             
 
                 
