cc27ebe86d37f73f82ae3103fb541bb46886a048
Gerald <157247952+Gerald450@users.noreply.github.com>
2025-11-15 20:31:58 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (35.58%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -1,18 +1,18 @@
 from math import factorial
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        nums = [i for i in range(1, n+1)]
+        nums = [i for i in range(1, n + 1)]
+        otp = []
+        k-=1
 
-        k -= 1
-
-        res = []
         for i in range(n, 0, -1):
             f = factorial(i - 1)
-            idx = k // f
+            pos = k // f
             k = k % f
-            res.append(str(nums.pop(idx)))
+            otp.append(str(nums.pop(pos)))
 
-        return "".join(res)
+        return ''.join(otp)
 
+            
 
         
