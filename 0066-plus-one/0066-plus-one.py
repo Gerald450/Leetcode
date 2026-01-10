f803853f7ba0916b976eec988b92751b9d1404d2
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-01-09 21:48:03 -0500

    Time: 0 ms (100%), Space: 19.4 MB (5.99%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,30 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits.reverse()
+
+        carry = 0
+        print(digits)
+
+        for idx, digit in enumerate(digits):
+            total = digit + carry 
+            if idx == 0:
+                total += 1
+
+
+            carry = total // 10
+            total = total % 10
+           
+
+            digits[idx] = total
+            #commit
+            
+
+        if carry:
+            digits.append(carry)
+
+        digits.reverse()
+
+        return digits
+
+
+        
