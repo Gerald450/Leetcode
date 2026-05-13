a8a7fdc725bb3b928c3c51cdb3cb675df34d497c
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-05-13 11:31:50 -0500

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        input: nums
+        output: array of indices
+
+        edge case: 
+
+        plan:
+            loop through arr
+                calculate the compliment of the element
+                if compliment in dic, return the value of that key alongside the curr index
+            insert curr index in dic
+
+        '''
+
         hashmap = {}
 
         for idx, num in enumerate(nums):
-            comp = target - num
-            if comp in hashmap:
-                return [hashmap[comp], idx]
+            compliment = target - num
+            if compliment in hashmap:
+                return [hashmap[compliment], idx]
             hashmap[num] = idx
         
