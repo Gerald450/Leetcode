d1dc33954021ed577543638cf6e42d56b0ad6cad
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-05-13 13:38:19 -0400

    Time: 8 ms (33.06%) | Memory: 20.1 MB (66.61%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,49 +1,29 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         '''
-
-
-        edge: empty, negative numbers
+        input: array of nums
+        output: min num of jumps
+        edge cases: empty, negative nums
 
         plan:
-        find the farthest jump, take that one and increment count
-        [2, 3, 1, 1, 4, 5, 6, 2]
-
-        need two pointer to show the window
-        on each index find the window it just to
-        in that window find the furthest jump and update your point to point,
-        furthest jump window and increment count
-        return count
+        form jump windows
+        farthest each window can jump
+        [2, 3, 3, 5, 4, 1]
+        [ , [ , ],[ , , ]]
+        use two pointers to maintain the window
         '''
 
+        
+        jumps = 0
         l, r = 0, 0
-        count = 0
 
         while r < len(nums) - 1:
-            furthest = 0
+            farthest = 0
             for i in range(l, r + 1):
-                furthest = max(furthest, i + nums[i])
-
+                farthest = max(farthest, i + nums[i])
+            
             l = r + 1
-            r = furthest
-            count += 1
+            r = farthest
+            jumps += 1
 
-        return count
-
-        '''
-        [2,3,1,1,4]
-
-        l = 2
-        r = 4 -> 4
-        count = 2
-        window = 4 - 2 + 1 = 3
-        furthest = 4
-        i = 2 -> 2
-        l + nums[1] = 1 + 3 == 4 > furthest
-        start = 2
-
-        
-        
-
-
-        '''
+        return jumps
