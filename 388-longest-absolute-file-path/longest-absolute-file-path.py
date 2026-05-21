c2549b43b696925b5c55104a5f4b89b1c236a51e
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-05-21 14:02:23 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.35%) - LeetSync

---
+++ 388-longest-absolute-file-path/longest-absolute-file-path.py
@@ -0,0 +1,59 @@
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        '''
+        input: string of filepath
+        output: length of longest past
+
+        edge: no file
+
+        plan:
+        use stack
+        split by \n
+        pop if we on same level
+        num_items_to_pop = size of stack - curr_level
+        add if we going deeper
+        keep track of max size
+        '''
+        def count(strs):
+            count = 0
+            for string in strs:
+                if string == '\t':
+                    count += 1
+            return count
+
+
+        input_arr = input.split('\n')
+        stack = []
+        maxlength = 0
+        curr_level = 0
+
+        for dir in input_arr:
+            
+            incoming = count(dir)
+            #remove trailing \t's
+            dir = dir.strip('\t')
+            if incoming > curr_level:
+                stack.append(dir)
+                if '.' in dir:
+                    length = len(''.join(stack)) + len(stack) - 1
+                    maxlength = max(maxlength, length)
+            else:
+                pops = len(stack) - incoming
+                for _ in range(pops):
+                    stack.pop()
+                stack.append(dir)
+                if '.' in dir:
+                    length = len(''.join(stack)) + len(stack) - 1
+                    maxlength = max(maxlength, length)
+            curr_level = incoming
+
+            
+
+        return maxlength
+
+        '''
+        input_arr = [file1.txt, file2.txt, longfile.txt]
+
+
+        '''
+                
