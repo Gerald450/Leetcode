6deb2d239f709de1b70edb6648ce187143beb2b8
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-05-24 18:12:48 -0400

    Time: 331 ms (79.85%) | Memory: 45.3 MB (25.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,15 +1,59 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        rows = len(text1)
-        cols = len(text2)
-        dp = [[0] * (cols + 1) for r in range(rows + 1) ]
+        '''
+        input: text1: str, text:str
+        output: LCS: int
 
-        for r in range(rows - 1, -1, -1):
-            for c in range(cols - 1, -1, -1):
-                if text1[r] == text2[c]:
-                    dp[r][c] = 1 + dp[r + 1][c + 1]
+        edge: empty str on either, no common subsequence
+
+        plan:
+        2dp, bottom-up
+        create a 2d grid with rows length of text1 + 1, and columns length of text2 + 2
+        extra row and column are for edge comparisons and will be initialised to zeros
+        if two letters match, update grid to 1+ number in diagonal
+        if they are not, take the minimum of grid bottom and one to the right
+        return grid at top corner
+        '''
+
+        rows, cols = len(text1) + 1, len(text2) + 1
+        '''
+        [[3,4,5,5,2]
+        [0,0,0,0,0]]
+
+        '''
+        dp = [[0 for _ in range(cols)] for _ in range(rows)] 
+
+
+       
+        for i in range(rows - 2, -1, -1):
+            for j in range(cols - 2, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
                 else:
-                    dp[r][c] = max(dp[r + 1][c], dp[r][c + 1])
-        
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+
+
         return dp[0][0]
 
+
+        '''
+          a. c. b.
+       [a[2, 1, 1, 0], 
+        b[1, 1, 1, 0], 
+        c[1, 1, 0, 0], 
+         [0, 0, 0, 0]]
+        
+        rows = 4 - 2 = 2
+        cols = 4 - 2 = 2
+        text1[2] = c
+        text2[2] = b
+        text2[1] = c
+        text2[0] = a
+
+
+        i = 1
+        text1[1] = b
+        
+        '''
+        
