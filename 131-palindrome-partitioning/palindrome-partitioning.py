559c6056e1e5166b9246c12c9ea308df3870ffc4
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-06-05 10:00:49 -0500

    Time: 53 ms (17.85%) | Memory: 34.1 MB (61.67%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,58 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        '''
+        input: string
+        output: list of list of palindromes
+
+        edge: no palidromes, uppercase, length is one
+
+        plan:
+        use backtrack
+        check is substring is a palindrome if so, append to current path
+        and pop after backtracking
+        start new path at end + 1
+        '''
+
+        def is_palindrome(word):
+            l, r = 0, len(word) - 1
+            while l <= r:
+                if word[l] != word[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        path = []
+        output = []
+
+        def backtrack(start):
+            if start == len(s):
+                output.append(path[:])
+
+            for end in range(start, len(s)):
+                word =  s[start: end + 1]
+                if is_palindrome(word):
+                    path.append(word)
+                    backtrack(end + 1)
+                    path.pop()
+
+
+        backtrack(0)
+
+        return output
+
+
+
+        '''
+        'aab'
+        path = []
+        output = []
+
+        end: 0 -> 3
+        word = s[0:1] = a
+        True
+        backtrack(1)
+        end: 1 -> 
+
+
+        '''
