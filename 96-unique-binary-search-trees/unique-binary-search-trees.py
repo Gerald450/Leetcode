1d1a0c7fb9d31470497cb5c5d509782dc006b6a4
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-07-29 14:31:26 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.71%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -0,0 +1,49 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        '''
+        input: n: int
+        output: int
+
+        edge: n == 1
+
+        plan:
+        use recursion
+        try every possible
+        count all left possibillities
+        count all right possibilities, multiply them and add them to total
+        '''
+
+
+        cache = {}
+        def count(nodes):
+            if nodes <= 1:
+                return 1
+            if nodes in cache:
+                return cache[nodes]
+            
+            ways = 0
+            for root in range(1, nodes + 1):
+                left = root - 1
+                right = nodes - root
+
+                ways += count(left) * count(right)
+            
+            cache[nodes] = ways
+            return ways
+
+
+        return count(n)
+
+            
+
+            
+
+
+
+
+       
+            
+
+
+
+        
