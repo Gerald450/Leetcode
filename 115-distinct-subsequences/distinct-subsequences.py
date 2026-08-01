1a978a868e96fdfd147832b94b7c8977e1178601
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-08-01 14:30:17 -0500

    Time: 887 ms (10.11%) | Memory: 232.7 MB (11.79%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,57 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        '''
+        input: s and t: str
+        output: ways: int
+
+        edge: s or t are empty, or both, non engl, s < t
+
+        plan:
+        recursion with memoization
+        store a cache with i, j indices for curr i and j in s and t
+        how many ways t[j:] from s[i:]
+        {
+            parameter => s, t
+            base: if j out of bounds: return 1
+            if i out of bounds: return 0
+            if (i,j) in cache: return cache[(i,j)]
+            
+            if chars at i and j are equal: ways += recursively call with j+1, i+1, dont take
+            if not equal: ways += call with i + 1, j
+
+            cache[(i,j)] = ways
+        }
+        call recursive function
+        return ways
+        '''
+
+        cache = {}
+    
+    
+        def find_ways(i, j):
+            
+            if j >= len(t): return 1
+            if i >= len(s): return 0
+
+            if (i, j) in cache: return cache[(i, j)]
+            ways = 0
+            if s[i] == t[j]:
+
+                #take
+                ways += find_ways(i + 1, j + 1)
+
+                #dont take
+                ways += find_ways(i + 1, j)
+
+            else:
+                ways += find_ways(i + 1, j)
+
+            cache[(i, j)] = ways
+            return ways
+
+        return find_ways(0, 0)
+
+
+        '''
+
+        '''
