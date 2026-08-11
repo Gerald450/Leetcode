a0fceee42d5f6bbd807aafb2c2a15b0040094e55
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-08-10 22:01:29 -0500

    Time: 3 ms (36.38%) | Memory: 19.4 MB (67.30%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -0,0 +1,66 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        '''
+        input: str
+        output: list[int]
+
+        edge: no operator, negative num, empty
+
+        plan:
+        recursion
+        treat each operator as the final operator
+        split left, and right and get their outcomes and add them to global list
+
+
+        [2, 3, 4], [4, 5]
+        [8, 10, 12, 15, 16, 20]
+        '''
+
+
+        def dfs(partial):
+            if partial.isnumeric():
+                return [int(partial)]
+
+            res = []
+            for idx, char in enumerate(partial):
+                if char == "+":
+                    left = dfs(partial[:idx])
+                    right = dfs(partial[idx + 1:])
+                    for i in range(len(left)):
+                        for j in range(len(right)):
+                            res.append(left[i] + right[j])
+
+                elif char == "-":
+                    left = dfs(partial[:idx])
+                    right = dfs(partial[idx + 1:])
+                    for i in range(len(left)):
+                        for j in range(len(right)):
+                            res.append(left[i] - right[j])
+                
+                elif char == "*":
+                    left = dfs(partial[:idx])
+                    right = dfs(partial[idx + 1:])
+                    for i in range(len(left)):
+                        for j in range(len(right)):
+                            res.append(left[i] * right[j])
+
+           
+
+            return res
+          
+        output = dfs(expression)
+        return output
+
+        '''
+        "2-1-1"
+          ^
+
+        left = "2" => [2]
+        right = "1-1" => [0]
+                  ^
+            left = [1]
+            right = [1]
+            res = 0
+
+        '''
+        
