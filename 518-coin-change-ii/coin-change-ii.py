79ace7a97b92b0116eb2e8eb93d68b9d789462cf
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-08-13 01:26:58 -0500

    Time: 303 ms (58.32%) | Memory: 19.6 MB (53.96%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        '''
+        input: amount, coins[int]
+        output: int
+
+        edge: amount is unreachable, empty list, amout == 0 or negative, dups
+
+        plan:
+        use dp memoization
+        start from amount == 0 and work my way up
+        calculate by looping through coins, subtract from curr amount and use value stored in dp
+        '''
+
+        dp = [0 for _ in range(amount + 1)]
+        dp[0] = 1
+        
+        for coin in coins:
+            for curr in range(amount + 1):
+                if curr - coin >= 0:
+                    dp[curr] += dp[curr - coin]
+        
+        print(dp)
+        return dp[amount]
+
+        '''
+
+        dp = [0, 1, 0, 0, 0, 0]
+                 ^
+        curr = 1 -> 6
+            coin = 1
+                1 - 1 = 0
+                dp[1] = 1 + dp[0] = 1
+        curr = 2 -> 6
+            coin = 1
+                2 - 1 = 1
+                dp[2] = 1 + dp[1] = 2
+            coin = 2
+                2 - 2 = 0
+                dp[2] = 1 + dp[0] = 1
+
+        '''
+
+        
