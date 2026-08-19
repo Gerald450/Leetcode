2d28856962030fec892d028e64cf607e0df4d1da
Gerald <157247952+Gerald450@users.noreply.github.com>
2026-08-18 21:12:12 -0500

    Time: 3 ms (45.04%) | Memory: 20 MB (11.15%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,20 +1,19 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         
-
+        cache = {}
         def dfs(l, r) -> int:
             if l == r:
                 return nums[l]
+            if (l, r) in cache:
+                return cache[(l, r)]
 
             take_left = nums[l] - dfs(l + 1, r)
             take_right = nums[r] - dfs(l, r - 1)
 
+            cache[(l, r)] = max(take_left, take_right)
+
             return max(take_left, take_right)
-          
-
-
-            
-        
 
 
         return dfs(0, len(nums) - 1) >= 0
